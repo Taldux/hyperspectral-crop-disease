6c3851d77124bb44d90ea2b1ced0a95d9bf358af
Taldux <taldux007@gmail.com>
2026-03-28 17:21:15 +0100

    fix module exports

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
 from .classifier import HybridCNNTransformer
+from .flow import ConditionalGlow, glow_nll_loss
 
-__all__ = ["HybridCNNTransformer"]
+__all__ = ["HybridCNNTransformer", "ConditionalGlow", "glow_nll_loss"]
